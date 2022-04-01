7279dbd68de6198672f2e1dbfa53eb4311bf861b
sameeramin2015 <sameeramin2015@gmail.com>
2022-04-01 13:32:00 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
-
-import NewsList from './components/NewsList';
+import React, { useEffect, useState } from 'react'
 
 function App() {
+  const [getNews,setNews] =useState([])
+  useEffect(()=>{
+    const fetchItems = async()=>{
+      let res = await fetch('/v2/everything?q=tesla&from=2022-03-01&sortBy=publishedAt&apiKey=ac8165351ca14f20a473e28e7492c919')
+      let data = await res.json()
+      setNews(data)
+
+    }
+    fetchItems()
+  })
   return (
     <div className="App">
      
+     {console.log(getNews)}
      
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,31 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import Header from './components/Header';
-import Footer from './components/Footer';
-import WeatherList from './components/WeatherList';
-import SportList from './components/SportList';
-import NewsList from "./components/NewsList";
-import Technologylist from "./components/TechnologyList";
-import TravelList from "./components/TravelList";
 
 
+import App from './App'
 
 
 
 
 ReactDOM.render(
   <BrowserRouter>
-    <Header />
+    {/* <Header /> */}
     <Routes>
 
-    <Route path="/" element={<NewsList />} />
-    <Route path="/weatherlist" element={<WeatherList/>}/>
+    <Route path="/" element={<App />} />
+    {/* <Route path="/weatherlist" element={<WeatherList/>}/>
     <Route path="/sportlist" element={<SportList />} />
     <Route path="/technologylist" element={<Technologylist />} />
-    <Route path="/travellist" element={<TravelList />} />
+    <Route path="/travellist" element={<TravelList />} /> */}
     </Routes>
-    <Footer/>
+    {/* <Footer/> */}
   </BrowserRouter>,
   document.getElementById('root')
 );
