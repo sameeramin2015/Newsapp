3f2af4ab9e44c8e5cde44634c02c1bc5e552304c
sameeramin2015 <sameeramin2015@gmail.com>
2022-04-01 11:26:37 +0200

    test commit

--- src/components/WeatherList.js
+++ src/components/WeatherList.js
@@ -13,7 +13,7 @@ const NewsList = () => {
 
     useEffect (() => {
         const getArticles = async () => {
-        const response = await axios.get('https://newsapi.org/v2/everything?q=weather&apiKey=ac8165351ca14f20a473e28e7492c919'); 
+        const response = await axios.get('v2/everything?q=weather&apiKey=ac8165351ca14f20a473e28e7492c919'); 
         console.log(response)
         setArticles(response.data.articles)
         }
@@ -21,7 +21,7 @@ const NewsList = () => {
     }, [])
   return (
     <div>
-        this is weather
+        
       {articles.map(article =>{
             return(
              <NewsItem 
